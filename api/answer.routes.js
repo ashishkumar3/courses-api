613dd8a1610f0a38b05fd854986c6e9ed0ef767c
ashishkumar3 <ashishk4371@gmail.com>
2020-04-17 14:44:55 +0530

    comment on answer

--- api/answer.routes.js
+++ api/answer.routes.js
@@ -7,4 +7,7 @@ const answerController = require('../controller/answer.controller');
 // GET /api/v1/answers - Return all the answers in the database.
 router.get('/', answerController.getAllAnswers);
 
+// POST /api/v1/answers/:id/comment - Post comment on an answer
+router.post('/:id/comment', answerController.commentOnAnswer);
+
 module.exports = router;
--- controller/answer.controller.js
+++ controller/answer.controller.js
@@ -33,3 +33,33 @@ exports.getAllAnswers = async (req, res, next) => {
     }
 };
 
+// POST comment to an answer. POST /api/v1/answers/:id/comment
+exports.commentOnAnswer = async (req, res, next) => {
+
+    const answerId = req.params.id;
+    const user_id = req.user.id;
+
+    try {
+        const exists = await knex.schema.hasTable(tableNames.answer);
+
+        if (!exists) {
+            res.status(404);
+            next(new Error('I think you are lost!'));
+            return;
+        }
+
+        // const answers = await knex(tableNames.answer).select('*');
+        const comment = await knex(tableNames.comment).insert({
+            description: req.body.comment, rating: 0, user_id, answer_id: answerId
+        }).returning('*');
+
+        if (comment.length < 1) {
+            throw new Error('Something went wrong! Try again later.');
+        }
+        res.json(comment);
+
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+};
